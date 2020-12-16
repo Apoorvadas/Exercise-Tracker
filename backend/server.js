4741d898d35b62b8f35f62d7172c597443e3eca0
Apoorva Das <42551046+Apoorvadas@users.noreply.github.com>
2020-12-16 22:24:19 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
+const uri = 'mongodb://127.0.0.1:27017/exercise';
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
 );
 const connection = mongoose.connection;
